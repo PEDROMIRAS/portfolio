a35831a2e4bd4a4d8a9e63e78b10d5292fd465b3
Pedro <p.miraspc35@gmail.com>
2025-10-02 16:15:00 +0200

    mod

---
+++ js/contact.js
@@ -0,0 +1,20 @@
+//===== Social Contact =====//
+// Email
+const emailBtn = document.getElementById('email');
+function openEmail(){
+    window.open(" ","_blank");
+};
+emailBtn.addEventListener('click', openEmail);
+//LinkedIn
+const linkedInBtn = document.getElementById('linkedIn');
+function openLinkedIn(){
+    window.open(" ","_blank");
+};
+linkedInBtn.addEventListener('click', openLinkedIn);
+//Discord
+const discordBtn = document.getElementById('discord');
+function openDiscord(){
+    window.open(" ","_blank");
+}
+discordBtn.addEventListener('click', openDiscord);
+//Fin contact.js
